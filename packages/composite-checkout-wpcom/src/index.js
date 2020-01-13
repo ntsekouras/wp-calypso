aeee2eb5194e540005409bf6cef543b686781e4f
nbloomf <nathan.bloomfield@automattic.com>
2020-01-13 12:06:21 -0600

    Add WPCheckoutErrorBoundary to public API

--- packages/composite-checkout-wpcom/src/index.js
+++ packages/composite-checkout-wpcom/src/index.js
@@ -2,9 +2,17 @@
  * Internal dependencies
  */
 import WPCheckout from './components/wp-checkout';
+import WPCheckoutErrorBoundary from './components/wp-checkout-error-boundary';
 import { useShoppingCart } from './hooks/use-shopping-cart';
 import { useWpcomStore } from './hooks/wpcom-store';
 import { mockSetCartEndpoint, mockGetCartEndpointWith } from './mock/cart-endpoint';
 
 // Re-export the public API
-export { WPCheckout, useShoppingCart, useWpcomStore, mockSetCartEndpoint, mockGetCartEndpointWith };
+export {
+	WPCheckout,
+	WPCheckoutErrorBoundary,
+	useShoppingCart,
+	useWpcomStore,
+	mockSetCartEndpoint,
+	mockGetCartEndpointWith,
+};
